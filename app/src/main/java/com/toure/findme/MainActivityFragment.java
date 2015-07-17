81f8cecb48a073ee1c18887bfd997d2b5321d137
tourenathan <teyou21@gmail.com>
2015-07-17 22:24:21 +0100

    Tracking the state of the application when the fragment is destroy and recreated

--- app/src/main/java/com/toure/findme/MainActivityFragment.java
+++ app/src/main/java/com/toure/findme/MainActivityFragment.java
@@ -29,10 +29,22 @@ public class MainActivityFragment extends Fragment {
     TextView alt;
     TextView time;
 
+    //Key to keep the location object when this fragment is recreated from a save state
+    private static final String LOCATION_KEY = "location_key";
+
+    //Key to keep the location time when this fragment is recreated from a save state
+    private static final String LAST_UPDATED_TIME_STRING_KEY = "last_updated_time";
+
+
     public MainActivityFragment() {
 
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        updateValuesFromBundle(savedInstanceState);
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -48,7 +60,7 @@ public class MainActivityFragment extends Fragment {
         return rootView;
     }
 
-//    @Override
+    //    @Override
 //    public void onLocationChanged(Location location) {
 //        mLastLocation = location;
 //        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
@@ -66,14 +78,42 @@ public class MainActivityFragment extends Fragment {
         alt.setText(Double.toString(mLastLocation.getAltitude()));
 
         //Get the current time and display
-        time.setText(DateFormat.getTimeInstance().format(new Date()));
+        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+        time.setText(mLastUpdateTime);
     }
 
-    public void setLocation(Location location){
+    public void setLocation(Location location) {
         mLastLocation = location;
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putParcelable(LOCATION_KEY, mLastLocation);
+        outState.putString(LAST_UPDATED_TIME_STRING_KEY, mLastUpdateTime);
+        super.onSaveInstanceState(outState);
+    }
 
 
+    //Method used to get the values from the saved Instance state
+    private void updateValuesFromBundle(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
 
+
+            // Update the value of mCurrentLocation from the Bundle and update the
+            // UI to show the correct latitude and longitude.
+            if (savedInstanceState.keySet().contains(LOCATION_KEY)) {
+                // Since LOCATION_KEY was found in the Bundle, we can be sure that
+                // mLastLocation is not null.
+                mLastLocation = savedInstanceState.getParcelable(LOCATION_KEY);
+            }
+
+            // Update the value of mLastUpdateTime from the Bundle and update the UI.
+            if (savedInstanceState.keySet().contains(LAST_UPDATED_TIME_STRING_KEY)) {
+                mLastUpdateTime = savedInstanceState.getString(
+                        LAST_UPDATED_TIME_STRING_KEY);
+            }
+            updateUI();
+        }
+
+    }
 }
